8744a5ef61ffaa2bf8a93ad0a4b6a63bfbfbb5af
diogo-guimaraes <diogoguimaraes.br@hotmail.com>
2026-05-14 09:53:49 -0400

    att

--- src/Services/api.ts
+++ src/Services/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import * as SecureStore from "expo-secure-store";
 
 export const api = axios.create({
-  baseURL: "http://192.168.100.6/api",
+  baseURL: process.env.EXPO_PUBLIC_API_URL,
   headers: {
     Accept: "application/json",
     "Content-Type": "application/json",
@@ -11,20 +11,15 @@ export const api = axios.create({
 
 api.interceptors.request.use(
   async (config) => {
-
-    const token =
-      await SecureStore.getItemAsync(
-        "token"
-      );
+    const token = await SecureStore.getItemAsync("token");
 
     if (token) {
-      config.headers.Authorization =
-        `Bearer ${token}`;
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
   },
   (error) => {
     return Promise.reject(error);
-  }
+  },
 );
